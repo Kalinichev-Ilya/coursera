9861941413235151feba7ebc1c402119507d69ea
Kalinichev Ilya <iluya.kalinichev@gmail.com>
2017-07-01 01:11:40 +0300

    refactoring

--- fullstack_ror/recipefinder/app/models/recipe.rb
+++ fullstack_ror/recipefinder/app/models/recipe.rb
@@ -1,9 +1,11 @@
 class Recipe
   include HTTParty
-
+  
+  key = ENV['FOOD2FORK_KEY']
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  
   base_uri "http://#{hostport}/api"
-  default_params  key: ENV['FOOD2FORK_KEY']
+  default_params  key: key
   format :json
   
   def self.for(term)
