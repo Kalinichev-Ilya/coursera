c63a1c0381bee92e08834baefe00df8745c8ec0f
Kalinichev Ilya <iluya.kalinichev@gmail.com>
2017-07-01 13:08:50 +0300

    refactoring, delete timezone file

--- fullstack_ror/recipefinder/app/models/recipe.rb
+++ fullstack_ror/recipefinder/app/models/recipe.rb
@@ -1,14 +1,14 @@
 class Recipe
   include HTTParty
   
-  key = ENV['FOOD2FORK_KEY']
+  key_value = ENV['FOOD2FORK_KEY']
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   
   base_uri "http://#{hostport}/api"
-  default_params  key: key
+  default_params  key: key_value
   format :json
   
-  def self.for(term)
-    get('/search', query: { q: term })['recipes']
+  def self.for(keyword)
+    get('/search', query: { q: keyword})['recipes']
   end
 end
--- fullstack_ror/recipefinder/config/environment.rb
+++ fullstack_ror/recipefinder/config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-ENV['FOOD2FORK_KEY'] = '8343ba866832d7ed6c38fdcc15abaa69'
-ENV['FOOD2FORK_SERVER_AND_PORT'] = 'www.food2fork.com'
-
 # Initialize the Rails application.
 Rails.application.initialize!
